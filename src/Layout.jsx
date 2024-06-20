2b3a4897db0b9da89154f32ef62026e79f047638
babyGirl <kabutemuthoni@gmail.com>
2024-06-20 16:44:29 +0300

    Page layouts

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,8 +5,9 @@ export default function Layout() {
     <div className="layout">
       <Box sx={{ backgroundColor: 'red'}} className="topNavbar">Top navigation goes here...</Box>
       <Box sx={{ backgroundColor: 'green'}} className="sideNavigation">Side navigation goes here...</Box>
-      <Box sx={{ backgroundColor: 'hotpink'}}>Main box goes here...</Box>
-      <Box sx={{ backgroundColor: 'blue'}}>Aside box goes here...</Box>
+      <Box sx={{ backgroundColor: 'purple'}} className="watchList">Watch list box goes here...</Box>
+      <Box sx={{ backgroundColor: 'hotpink'}} className="mainBox">Main box goes here...</Box>
+      <Box sx={{ backgroundColor: 'blue'}} className="asideBox">Aside box goes here...</Box>
     </div>
   );
 }
