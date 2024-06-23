63ff1bbf9e906f141de74c8e121cef03eb08a7c3
Tom-Sarfo <tomsarfodavis@gmail.com>
2024-06-23 13:21:11 +0000

    Add height and border for layout sections

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,11 +3,19 @@ import { Box } from "@mui/material";
 export default function Layout() {
   return (
     <div className="layout">
-      <Box sx={{ backgroundColor: 'red'}} className="topNavbar">Top navigation goes here...</Box>
-      <Box sx={{ backgroundColor: 'green'}} className="sideNavigation">Side navigation goes here...</Box>
-      <Box sx={{ backgroundColor: 'purple'}} className="watchList">Watch list box goes here...</Box>
-      <Box sx={{ backgroundColor: 'hotpink'}} className="mainBox">Main box goes here...</Box>
-      <Box sx={{ backgroundColor: 'blue'}} className="asideBox">Aside box goes here...</Box>
+      <Box sx={{ borderBottom: 2 }} className="topNavbar">
+        Top navigation goes here...
+      </Box>
+      <Box sx={{ borderRight: 2 }} className="sideNavigation">
+        Side navigation goes here...
+      </Box>
+      <Box sx={{ borderBottom: 2 }} className="watchList">
+        Watch list box goes here...
+      </Box>
+      <Box className="mainBox">Main box goes here...</Box>
+      <Box sx={{ borderLeft: 2 }} className="asideBox">
+        Aside box goes here...
+      </Box>
     </div>
   );
 }
