0a6bc30698091b3bf9206ba65347efa51f7854d8
Tom-Sarfo <tomsarfodavis@gmail.com>
2024-06-29 00:34:58 +0000

    Merge watchlist layout into Main layout

---
+++ src/Components/MenuCard.jsx
@@ -0,0 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import PropTypes from "prop-types";
+import { ActiveBar } from "./Styled";
+
+function MenuCard({ menuTitle, icon, styles, active, onClick }) {
+  return (
+    <MenuList
+      sx={styles}
+      style={{
+        display: "flex",
+        alignItems: "center",
+      }}
+      onClick={onClick}
+    >
+      <ActiveBar active={active} />
+      <MenuItem
+        sx={{
+          display: "flex",
+          alignItems: "baseline",
+          "&:hover": {
+            paddingRight: "auto",
+            transition: "0.3s ease",
+          },
+        }}
+      >
+        <ListItemIcon>
+          <FontAwesomeIcon icon={icon} size="sm" />
+        </ListItemIcon>
+        <ListItemText>{menuTitle}</ListItemText>
+      </MenuItem>
+    </MenuList>
+  );
+}
+
+MenuCard.propTypes = {
+  menuTitle: PropTypes.string.isRequired,
+  icon: PropTypes.object.isRequired,
+  styles: PropTypes.object,
+  active: PropTypes.bool.isRequired,
+  onClick: PropTypes,
+};
+
+export default MenuCard;
