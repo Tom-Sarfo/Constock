bd863f97f78a93845046dbef5791ef07d464b2a2
Tom-Sarfo <tomsarfodavis@gmail.com>
2024-07-10 06:21:25 +0000

    Make Table component reusable

--- src/Components/TableComponent.jsx
+++ src/Components/TableComponent.jsx
@@ -1,63 +1,17 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { Title } from "./Styled";
+import PropTypes from "prop-types";
 
-const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
-export default function ColumnGroupingTable() {
+function TableComponent({columns, rows}) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -71,13 +25,15 @@ export default function ColumnGroupingTable() {
   };
 
   return (
-    <Paper sx={{ width: '100%' }}>
+    <Paper
+      sx={{ width: "100%", boxShadow: "none", border: "1px solid #CCCCCC" }}
+    >
       <TableContainer sx={{ maxHeight: 440 }}>
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
-              <TableCell align="center" colSpan={2}>
-                Country
+              <TableCell align="left" colSpan={2}>
+                <Title label="My Assets" />
               </TableCell>
               <TableCell align="center" colSpan={3}>
                 Details
@@ -90,7 +46,7 @@ export default function ColumnGroupingTable() {
                   align={column.align}
                   style={{ top: 57, minWidth: column.minWidth }}
                 >
-                  {column.label}
+                  <Title label={column.label} sx={{ fontSize: "14px" }} />
                 </TableCell>
               ))}
             </TableRow>
@@ -105,9 +61,7 @@ export default function ColumnGroupingTable() {
                       const value = row[column.id];
                       return (
                         <TableCell key={column.id} align={column.align}>
-                          {column.format && typeof value === 'number'
-                            ? column.format(value)
-                            : value}
+                          {column.format ? column.format(value) : value}
                         </TableCell>
                       );
                     })}
@@ -129,3 +83,11 @@ export default function ColumnGroupingTable() {
     </Paper>
   );
 }
+
+TableComponent.propTypes = {
+  columns: PropTypes.array.isRequired,
+  rows: PropTypes.array.isRequired,
+};
+
+
+export default TableComponent;
--- src/Routes/Dashboard.jsx
+++ src/Routes/Dashboard.jsx
@@ -1,9 +1,65 @@
-// import PropTypes from "prop-types";
 import ArrowScroll from "../Components/ArrowScroll";
 import { useRef, useState } from "react";
 import { scrollToWidth } from "../Utils/Constant";
-import { Title, WatchListCardContainer } from "../Components/Styled";
+import { Title } from "../Components/Styled";
 import WatchList from "../Components/WatchList";
+import TableComponent from "../Components/TableComponent";
+
+const columns = [
+  {
+    id: "name",
+    label: "Name",
+    minWidth: 170,
+    format: (value) => <p style={{ color: "Red" }}>{value.toUpperCase()}</p>,
+  },
+  { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+  {
+    id: "population",
+    label: "Population",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "size",
+    label: "Size\u00a0(km\u00b2)",
+    minWidth: 170,
+    align: "right",
+    format: (value) => (
+      <p style={{ color: "red" }}>{value.toLocaleString("en-US")}</p>
+    ),
+  },
+  {
+    id: "density",
+    label: "Density",
+    minWidth: 170,
+    align: "right",
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData("India", "IN", 1324171354, 3287263),
+  createData("China", "CN", 1403500365, 9596961),
+  createData("Italy", "IT", 60483973, 301340),
+  createData("United States", "US", 327167434, 9833520),
+  createData("Canada", "CA", 37602103, 9984670),
+  createData("Australia", "AU", 25475400, 7692024),
+  createData("Germany", "DE", 83019200, 357578),
+  createData("Ireland", "IE", 4857000, 70273),
+  createData("Mexico", "MX", 126577691, 1972550),
+  createData("Japan", "JP", 126317000, 377973),
+  createData("France", "FR", 67022000, 640679),
+  createData("United Kingdom", "GB", 67545757, 242495),
+  createData("Russia", "RU", 146793744, 17098246),
+  createData("Nigeria", "NG", 200962417, 923768),
+  createData("Brazil", "BR", 210147125, 8515767),
+];
 
 function Dashboard() {
   const [scrollToPosition, setScrollToPosition] = useState(0);
@@ -14,7 +70,7 @@ function Dashboard() {
         padding: "1.2rem",
         display: "flex",
         flexDirection: "column",
-        gap: "20px",
+        gap: "40px",
       }}
     >
       <div>
@@ -34,9 +90,9 @@ function Dashboard() {
           )
         }
       />
+      <TableComponent columns={columns} rows={rows} />
     </div>
   );
 }
-
 
 export default Dashboard;
